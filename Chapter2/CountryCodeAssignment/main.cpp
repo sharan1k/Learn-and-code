8f6f90535c1de849571a47d621a4792a0ee7007c
KM Sharan <km.sharan@intimetec.com>
2024-12-10 12:34:37 +0530

    implemented the console application that takes country codes and gives adjacent countries

---
+++ Chapter2/CountryCodeAssignment/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "CountryData.h"
+
+void toUpperCase(std::string& sentence) {
+    for (char& character : sentence) {
+        if (character >= 'a' && character <= 'z') {
+            character = character - 'a' + 'A';  
+        }
+    }
+}
+
+std::string getCountryName(const std::string& countryCode) {
+    auto countryCodeIterator = countryCodes.find(countryCode);
+    return (countryCodeIterator != countryCodes.end()) ? countryCodeIterator->second : "Country code not found.";
+}
+
+std::vector<std::string> getAdjacentCountries(const std::string& countryCode) {
+    auto countryNeighboursIterator = countryNeighbours.find(countryCode);
+    return (countryNeighboursIterator != countryNeighbours.end()) ? countryNeighboursIterator->second : std::vector<std::string>{};
+}
+
+
+int main() {
+    std::string countryCode;
+
+    while(true)
+    {
+        std::cout << "Enter the Country Code or 'Q' to quit: ";
+        std::cin >> countryCode;
+
+        toUpperCase(countryCode);
+
+        if (countryCode == "Q") {
+            std::cout << "Exiting program..." << std::endl;
+            break;
+        }
+
+        std::string countryName = getCountryName(countryCode);
+
+        if (countryName == "Country code not found.") {
+            std::cout << countryName << std::endl;
+            continue;
+        }
+
+        std::vector<std::string> neighbors = getAdjacentCountries(countryCode);
+        std::cout << "Country Name: " << countryName << std::endl;
+
+        if (neighbors.empty()) {
+            std::cout << "This country has no adjacent countries." << std::endl;
+        } else {
+            std::cout << "Adjacent Countries:" << std::endl;
+            for (const auto& neighbor : neighbors) {
+                std::cout << "- " << neighbor << std::endl;
+            }
+        }
+    }
+    
+    return 0;
+}
